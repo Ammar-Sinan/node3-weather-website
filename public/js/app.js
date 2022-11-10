f6b164593ab15c79af991813e646b822fa7abf70
Ammar Sinan <ammarsinan@pc-81.home>
2022-11-10 13:07:26 +0300

    Removed unnecessary things

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,3 @@
-console.log('Testing javascript')
-
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
